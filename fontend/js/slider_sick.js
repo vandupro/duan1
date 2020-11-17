3e5c5757f1f932ce013ee1bf84527af018a91b3d
phuong <dangxuong2000@gmail.com>
2020-11-17 14:33:36 +0700

    phuong_up

---
+++ fontend/js/slider_sick.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:4,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
