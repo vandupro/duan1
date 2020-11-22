6504fd04bc7cf8f0c965d2c5b06fcefd6ceb527b
phuong <dangxuong2000@gmail.com>
2020-11-23 04:26:52 +0700

    responsive-phuong

--- layouts/frondend/js/hidden-menu.js
+++ layouts/frondend/js/hidden-menu.js
@@ -1,9 +1,14 @@
+var width = screen.width; //chiều rộng mà hình
+var icon_menu = document.getElementById("cate"); //nút bấm
+var x = document.getElementById("hiden-none-menu"); //menu sẽ đc hiển thị
 
-    //     // var width = screen.width; //chiều rộng mà hình
-    //     var icon_menu = document.getElementById("cate");
-    //     var x = document.getElementById("menu_show");
-    //    // if(width <=992){
-    //    icon_menu.onclick= function(){
-    //        alert("Hello! I am an alert box!!");
-    //    x.classList.add("mystyle");
-    //    }
+icon_menu.onclick = function () {
+  if (width <= 992) {
+    if (x.style.display === "none") {
+      alert("Hello! I am an alert box!!");
+    }
+    else {
+        alert("chưa ẩn!");
+      }
+  }
+};
